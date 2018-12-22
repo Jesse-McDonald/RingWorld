83eaf9364c2ae76b43c6d582025849203cdcd0b6
Jesse McDonald <30709204+Jesse-McDonald@users.noreply.github.com>
2018-12-22 02:04:23 -0800

    fixed POC to work on my home pc
    also added a.out, do note a.out will only work if you have 1 1920x1080 screen surrounded by 2 1280x1024 monitors

--- POC.cpp
+++ POC.cpp
@@ -1,7 +1,7 @@
 // proof of concept basic application
 //
 
-#include "stdafx.h"
+//#include "stdafx.h"
 //#include "winuser.h"
 #include "windows.h"
 #include <iostream>
@@ -20,11 +20,13 @@ int main()
 		//cout <<(unsigned short) GetKeyState(VK_LBUTTON) << endl;
 		GetCursorPos(&pos);
 		//if (pos.x - pPos.x != 0 && pos.y - pPos.y != 0) {
-		//	std::cout << "(" << pos.x - pPos.x << "," << pos.y - pPos.y << ")" << std::endl;
+		//std::cout << "(" << pos.x - pPos.x << "," << pos.y - pPos.y << ")" << std::endl;
+		//std::cout << "(" << pos.x << "," << pos.y<< ")" << std::endl;
 		//}
 		dX = (dX / 2) + abs(pos.x - pPos.x);
 		dY = (dY / 2) + abs(pos.y - pPos.y);
-
+//std::cout << "(" << dX << "," << dY<< ")" << std::endl;
+		//}
 		if (abs(pos.x - pPos.x) > 0&& abs(pos.x - pPos.x)<1000) {
 
 
@@ -32,12 +34,12 @@ int main()
 				(GetKeyState(VK_LBUTTON) < 00)//mouse button is pressed, like if we are draging a window
 				||(dY!=0 && dX/(float)dY<1)//mouse moving at more that a 45* angle (probiably moving to a corner not edge)
 				)
-				|| dX>10) {//only "blink" if we have momentiom //change to using mouse raw
-				if (pos.x <= -1920) {
-					SetCursorPos(2559, pos.y);
+				&& dX>1) {//only "blink" if we have momentiom //change to using mouse raw
+				if (pos.x <= -1280) {
+					SetCursorPos(3199, pos.y);
 				}
-				else if (pos.x >= 2559) {
-					SetCursorPos(-1920, pos.y);
+				else if (pos.x >= 3199) {
+					SetCursorPos(-1280, pos.y);
 
 				}
 			}
